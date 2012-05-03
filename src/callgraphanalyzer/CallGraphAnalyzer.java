c30cd9897316c911a70e2d212add335295aa2ef2
infiro <inferno71123@yahoo.com>
2012-05-03 15:51:54 -0700

    Add db, need to test the connection and figure out what to get from db

---
+++ src/callgraphanalyzer/CallGraphAnalyzer.java
@@ -0,0 +1,80 @@
+package callgraphanalyzer;
+
+import java.io.IOException;
+
+import db.DbConnection;
+import models.File;
+import models.CallGraph;
+
+public class CallGraphAnalyzer {
+	
+	private DbConnection db = DbConnection.getInstance();
+	private CallGraph callGraphBefore = new CallGraph();
+	private CallGraph callGraphAfter  = new CallGraph();
+	
+	private String commitBeforeId;
+	private String commitAfterId;
+	private String dbName;
+	
+	private void initialize(String dbName, String commitBeforeId, String commitAfterId)
+	{
+		try
+		{
+			this.commitBeforeId = commitBeforeId;
+			this.commitAfterId  = commitAfterId;
+			this.dbName 		= dbName;
+			
+			// Connect to db, the DB must exist
+			db.connect(dbName);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Can not connect to db");
+		}
+	}
+	
+	/**
+	 * readDB
+	 * Initialize two callgraphs based on the two commits
+	 * @param db
+	 * @param commitBeforeId
+	 * @param commitAfterId
+	 * 
+	 */
+	public boolean readDB(String db, String commitBeforeId, String commitAfterId)
+	{
+		try
+		{
+			initialize(db, commitBeforeId, commitAfterId);
+			
+			//Parse commitBefore
+			initializeCallGraph(commitBeforeId, this.callGraphBefore);
+			
+			//Parse commitAfter
+			initializeCallGraph(commitAfterId, this.callGraphAfter);
+			
+			this.db.close();
+			return true;
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * Initialize callgraph
+	 * @param commitBeforeId
+	 * @param callgraph
+	 */
+	private void initializeCallGraph(String commitBeforeId, CallGraph callgraph)
+	{
+		// Clear callgraph
+		
+		// Read all commits from DB, inset to callgraph
+		
+	}
+		
+
+}
--- src/callgraphanalyzer/Main.java
+++ src/callgraphanalyzer/Main.java
@@ -1,16 +1,21 @@
 import java.io.IOException;
+import callgraphanalyzer.CallGraphAnalyzer;
 
 public class Main {
 
+	private static CallGraphAnalyzer callGraphAnalyzer = new CallGraphAnalyzer();
+	
 	/**
-	 * @param args
+	 * @param [dbname] [commit_before] [commit_after] 
 	 */
 	public static void main(String[] args) {
 		System.out.println("callGraphAnalyzer tool developed by eggnet.");
+		
 		try {
 			System.out.println(args.length);
-			if (args.length < 1)
+			if (args.length < 3 )
 			{
+				System.out.println("Retry: callGraphAnalyzer [dbname] [commit_before] [commit_after]");
 				throw new ArrayIndexOutOfBoundsException();
 			}
 			else
@@ -18,7 +23,7 @@ public class Main {
 				try 
 				{
 					// Read in scm2sql db input
-				
+					callGraphAnalyzer.readDB(args[0], args[1], args[2]);
 				} 
 				catch (Exception e) 
 				{
