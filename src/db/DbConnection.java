1b5e10d93c28bccd68abccae05a2d767a06a67a0
infiro <inferno71123@yahoo.com>
2012-05-02 17:22:24 -0700

    added db part, working progress'

---
+++ src/db/DbConnection.java
@@ -0,0 +1,97 @@
+package db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import callgraphanalyzer.Resources;
+
+public class DbConnection {
+
+	public static Connection conn = null;
+	public static DbConnection ref = null;
+	
+	private DbConnection() 
+	{
+		try 
+		{
+			Class.forName("org.postgresql.Driver").newInstance();
+		} 
+		catch (InstantiationException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IllegalAccessException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static DbConnection getInstance()
+	{
+		if (ref == null)
+			return (ref = new DbConnection());
+		else
+			return ref;
+	}
+	
+	/**
+	 * Executes a string of SQL on the current database
+	 * NOTE: this assumes your sql is valid.
+	 * @param sql
+	 * @return true if successful
+	 */
+	public boolean exec(String sql)
+	{
+		try {
+			PreparedStatement s = conn.prepareStatement(sql);
+			s.execute();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Connects to the given database.  
+	 * @param connectionString
+	 * @return true if successful
+	 */
+	public boolean connect(String connectionString)
+	{
+		try {
+			conn = DriverManager.getConnection(connectionString, Resources.dbUser, Resources.dbPassword);
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	/*
+	 * Close all connection
+	 */
+	public boolean close()
+	{
+		try {
+			conn.close();
+			return true;
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	
+}
