af585b40c83f3df0a99c80c623ce21705f0e381d
infiro <inferno71123@yahoo.com>
2012-05-14 13:50:42 -0700

    #28 test cases for callgraph getMethodsYUsingCharacters

--- test/testModels/TestCallGraph.java
+++ test/testModels/TestCallGraph.java
@@ -3,6 +3,7 @@ package testModels;
 import static org.junit.Assert.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import models.CallGraph;
@@ -16,6 +17,31 @@ public class TestCallGraph {
 	@Test
 	public void testGetMethodsUsingCharacters() 
 	{
+		CallGraph cg = generateDummyCallGraph();
+		
+		// Method starts at classNumber*100 + methodNumber*10
+		//		  end    at classNumber*100	+ methodNumber*10 + 10
+		
+		// File 2 has 2 classes, 1 method
+		List<Method> rgMethods = cg.getMethodsUsingCharacters("file_2", 0, 100);
+		assertEquals(rgMethods.size(), 1);
+		
+		// File 3 has 3 classes, 2 + 1 methods
+		rgMethods = cg.getMethodsUsingCharacters("file_3", 0,199);
+		assertEquals(rgMethods.size(), 1 ); //only class_1 methods
+		rgMethods = cg.getMethodsUsingCharacters("file_3", 200,299);
+		assertEquals(rgMethods.size(), 2 ); //only class_2 methods
+		rgMethods = cg.getMethodsUsingCharacters("file_3", 0,300);
+		assertEquals(rgMethods.size(), 3 ); //included class_2 methods
+		
+		
+		// File 4 has 4 classes, 3 + 2 + 1 methods
+		rgMethods = cg.getMethodsUsingCharacters("file_4", 0,500);
+		assertEquals(rgMethods.size(), 6 );
+	}
+	
+	public CallGraph generateDummyCallGraph()
+	{
 		CallGraph cg = new CallGraph();
 		
 		// dummy map
@@ -23,26 +49,48 @@ public class TestCallGraph {
 		Map<String, Clazz> 	clazzes = new HashMap<String, Clazz>();
 		Map<String, Method> methods = new HashMap<String, Method>();
 		
+		// File i has i-1 classes
+		// Each class j has j-1 methods
+		// ex: package_file_8.class_7.method_6 (start at line 60 end at line 70)
+		// Method starts at classNumber*100 + methodNumber*10
+		//		  end    at classNumber*100	+ methodNumber*10 + 10
 		for(int i=0; i< 10; i++)
 		{
-			String filename ="File_" + i;
 			File newFile = new File();
-			String packageName = "Package_" + filename;
-			for(int j =0; j<20; j++)
+			String filename ="file_" + i;
+			String packageName = "package_" + filename;
+			newFile.setFileName(filename);
+			
+			// classes
+			for(int j =0; j<i; j++)
 			{
 				Clazz c = new Clazz();
-				String className = "class" + j;
+				String className = "class_" + j;
 				
-				for(int k=0; k<10; k++)
+				// functions each
+				for(int k=0; k<j; k++)
 				{
 					Method m = new Method();
-					String methodName = String.format("%s.%s.method%s", packageName, className, i);
-					m.setName(methodName);
+					String methodName = "method_" + k;
+					String fullName = String.format("%s.%s.%s", packageName, className, methodName);
+					m.setName(fullName);
+					
+					// Startline = k, end = k+1;
+					m.setStartLine(j*100 + k*10);
+					m.setEndLine  (j*100 +(k+1)*10 );
+					c.addMethod(m);
 				}
+				
+				newFile.addClazz(c);
 			}
+			files.put(newFile.getFileName(),newFile);
 		}
 		
-		assertTrue(false);
+		cg.setFiles(files);
+		cg.setClazzes(clazzes);
+		cg.setMethods(methods);
+		
+		return cg;
 	}
 
 }
