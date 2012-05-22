d58e4fb27540503ac1f7d147cdc4ecf9419854c8
Braden Simpson <braden@uvic.ca>
2012-05-22 09:46:15 -0700

    #44, Ownership models

--- src/callgraphanalyzer/CallGraphAnalyzer.java
+++ src/callgraphanalyzer/CallGraphAnalyzer.java
@@ -4,11 +4,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import sun.security.krb5.internal.crypto.crc32;
-
 import models.CallGraph;
+import models.Change;
 import models.Method;
-import models.OwnerChange;
 import models.Relation;
 import models.User;
 import callgraphanalyzer.Comparator.CompareResult;
@@ -21,22 +19,28 @@ public class CallGraphAnalyzer
 	private CallGraph		callGraphBefore;
 	private CallGraph		callGraphAfte;
 	private Comparator		comparator;
-	
+
 	private String			commitBeforeId;
 	private String			commitAfterId;
 	private String			dbName;
 	private Set<Relation>	Relations;
 
-	public CallGraphAnalyzer() { 
+	public CallGraphAnalyzer()
+	{
 		this.Relations = new HashSet<Relation>();
 	}
-	
+
 	public void init(Comparator comp)
 	{
 		this.comparator = comp;
 		this.db = comp.db;
 	}
-	
+
+	public void generateLogicalOwnership()
+	{
+		
+	}
+
 	/**
 	 * This function will return all the methods that call the given method.
 	 * 
@@ -58,49 +62,48 @@ public class CallGraphAnalyzer
 		for (String modifiedFile : compareResult.modifiedFileMethodMap.keySet())
 		{
 			// For each method in the new methods.
-			for (Method newMethod : compareResult.modifiedFileMethodMap
-					.get(modifiedFile).newMethods)
+			for (Method newMethod : compareResult.modifiedFileMethodMap.get(modifiedFile).newMethods)
 			{
 				// get all methods this one is called by
-				OwnerChange newMethodChange = db.getLatestOwnerChange(modifiedFile, newMethod.getStartLine(), newMethod.getEndLine());
-				recurseMethods(new User().setUserEmail(newMethodChange.getOwnerId()), newMethod, CallGraphResources.METHOD_RECURSE_DEPTH, 0);
+				Change newMethodChange = db.getLatestOwnerChange(modifiedFile, newMethod.getStartLine(), newMethod
+						.getEndLine(), comparator.newCommit.getCommit_date());
+				recurseMethods(new User().setUserEmail(newMethodChange.getOwnerId()), newMethod,
+						CallGraphResources.METHOD_RECURSE_DEPTH, 0);
 			}
-			for (Method oldMethod : compareResult.modifiedFileMethodMap
-					.get(modifiedFile).oldMethods)
+			for (Method oldMethod : compareResult.modifiedFileMethodMap.get(modifiedFile).oldMethods)
 			{
 				if (compareResult.modifiedFileMethodMap.get(modifiedFile).newMethods.contains(oldMethod))
 					continue;
 				// get all methods this one is called by
-				OwnerChange newMethodChange = db.getLatestOwnerChange(modifiedFile, oldMethod.getStartLine(), oldMethod.getEndLine());
-				recurseMethods(new User().setUserEmail(newMethodChange.getOwnerId()), oldMethod, CallGraphResources.METHOD_RECURSE_DEPTH, 0);
+				Change newMethodChange = db.getLatestOwnerChange(modifiedFile, oldMethod.getStartLine(), oldMethod
+						.getEndLine(), comparator.oldCommit.getCommit_date());
+				recurseMethods(new User().setUserEmail(newMethodChange.getOwnerId()), oldMethod,
+						CallGraphResources.METHOD_RECURSE_DEPTH, 0);
 			}
 		}
 		compareResult.print();
 		for (Relation r : this.Relations)
 			r.print();
 	}
-	
+
 	public void recurseMethods(User changingUser, Method currentMethod, int maxDepth, int currentDepth)
 	{
-//		if (currentDepth == maxDepth)
-//			return;
-		
+		// if (currentDepth == maxDepth)
+		// return;
 		for (Method calledMethod : currentMethod.getCalledBy())
 		{
-			OwnerChange calledMethodChange = db.getLatestOwnerChange(calledMethod.getClazz().getFile().getFileName(), calledMethod.getStartLine(), calledMethod.getEndLine());					
+			Change calledMethodChange = db.getLatestOwnerChange(calledMethod.getClazz().getFile().getFileName(),
+					calledMethod.getStartLine(), calledMethod.getEndLine(), comparator.newCommit.getCommit_date());
 			boolean isSelf = false;
 			if (changingUser.getUserEmail().equals(calledMethodChange.getOwnerId()))
 				isSelf = true;
-			Relation r = new Relation()
-				.setPersonOne(new User().setUserEmail(changingUser.getUserEmail()))
-				.setPersonTwo(new User().setUserEmail(calledMethodChange.getOwnerId()))
-				.setWeight(1)
-				.setFileId(calledMethod.getClazz().getFile().getFileName())
-				.setCaller(calledMethod.getName())
-				.setCallee(currentMethod.getName())
-				.setIsSelf(isSelf);	// TEMP WEIGHT SETTING
+			Relation r = new Relation().setPersonOne(new User().setUserEmail(changingUser.getUserEmail()))
+					.setPersonTwo(new User().setUserEmail(calledMethodChange.getOwnerId())).setWeight(1)
+					// TEMP WEIGHT SETTING
+					.setFileId(calledMethod.getClazz().getFile().getFileName()).setCaller(calledMethod.getName())
+					.setCallee(currentMethod.getName()).setIsSelf(isSelf);
 			this.Relations.add(r);
-			recurseMethods(changingUser, calledMethod, maxDepth, currentDepth+1);
+			recurseMethods(changingUser, calledMethod, maxDepth, currentDepth + 1);
 		}
 	}
 }
--- src/db/CallGraphDb.java
+++ src/db/CallGraphDb.java
@@ -1,48 +1,10 @@
 package db;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import models.OwnerChange;
 
 public class CallGraphDb extends DbConnection
 {
 	public CallGraphDb()
 	{
 		super();
-	}
-
-	public OwnerChange getLatestOwnerChange(String fileId, int start, int end)
-	{
-		try
-		{
-//			String sql = "SELECT commit_id, file_id, owner_id, char_start, char_end, change_type FROM owners natural join commits where file_id=? AND "
-//					+ "((char_start<=? and char_end>=?) OR "
-//					+ "(char_start<=? and char_end>=?) OR "
-//					+ "(char_start>? and char_end<?) OR"
-//					+ "(char_end='-1' and change_type='ADD')) order by commit_date desc limit 1";
-			String sql = "SELECT commit_id, file_id, owner_id, char_start, char_end, change_type FROM owners natural join commits where file_id=? AND "
-					+ "(branch_id=? OR branch_id is NULL) order by commit_date desc";
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setString(1, fileId);
-//			stmt.setInt(2, start);
-//			stmt.setInt(3, start);
-//			stmt.setInt(4, end);
-//			stmt.setInt(5, end);
-//			stmt.setInt(6, start);
-//			stmt.setInt(7, end);
-			stmt.setString(2, branchID);
-			ResultSet rs = stmt.executeQuery();
-			if (!rs.next())
-				return null;
-			return new OwnerChange(rs.getString("owner_id"), rs.getString("commit_id"), Resources.ChangeType.valueOf(rs.getString("change_type")), rs.getString("file_id"), rs.getInt("char_start"), 
-						rs.getInt("char_end"));
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
 	}
 }
