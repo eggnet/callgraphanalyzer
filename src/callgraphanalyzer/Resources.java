a6f81877b6d8d5b9e476794da9d47447054b66da
Jordan Ell <jordan.ell7@gmail.com>
2012-05-28 13:32:47 -0700

    Added a few static functions to convert line and char numbers back and forth.

--- src/callgraphanalyzer/Resources.java
+++ src/callgraphanalyzer/Resources.java
@@ -30,5 +30,54 @@ public class Resources {
 		}
 		return index;
 	}
+	
+	public static int convertLineStartToCharStart(int lineStart, String file) {
+		int index = -1;
+		int lineCount = 1;
+		
+		for(char c: file.toCharArray()) {
+			if(c == '\n')
+				lineCount++;
+			else if(lineCount == lineStart) {
+				index++;
+				break;
+			}
+			index++;
+		}
+		
+		return index;
+	}
+	
+	public static int convertLineEndToCharEnd(int lineEnd, String file) {
+		int index = -1;
+		int lineCount = 1;
+		
+		for(char c: file.toCharArray()) {
+			if(c == '\n' && lineCount == lineEnd) {
+				index++;
+				break;
+			}
+			if(c == '\n')
+				lineCount++;
+			index++;
+		}
+		
+		return index;
+	}
+	
+	public static int convertCharToLine(int charNum, String file) {
+		int index = -1;
+		int lineCount = 1;
+		
+		for(char c: file.toCharArray()) {
+			index++;
+			if(index == charNum) 
+				break;
+			if(c == '\n')
+				lineCount++;
+		}
+		
+		return lineCount;
+	}
 
 }
