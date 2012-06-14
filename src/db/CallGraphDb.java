e5fcd34859a38ddf399bbc236e83191edc7c26c7
infiro <inferno71123@yahoo.com>
2012-06-14 16:05:46 -0700

    #78 rewrite GetOnwerForFileAtCommit to not use recursive db call

--- src/db/CallGraphDb.java
+++ src/db/CallGraphDb.java
@@ -3,11 +3,14 @@ package db;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import models.Change;
 import models.Commit;
+import models.CommitFamily;
 import models.User;
 
 
@@ -46,22 +49,68 @@ public class CallGraphDb extends DbConnection
 		}
 	}
 	
-	public List<Change> getAllFileOwnerChangesBefore(String FileId, String CommitId)
+	public Map<String, List<Change>> getAllFileOwnerChangesBefore(String FileId, String CommitId)
 	{
 		try 
 		{
-			LinkedList<Change> changes = new LinkedList<Change>();
 			String sql = "SELECT commit_id, file_id, owner_id, char_start, char_end, change_type FROM owners natural join commits where commit_date <= (select commit_date from commits where commit_id=?)" +
-					"and (branch_id is NULL OR branch_id=?) and file_id=? order by commit_date, commit_id, line_start;"; 
+					"and (branch_id is NULL OR branch_id=?) and file_id=? order by commit_id;"; 
 			String[] parms = {CommitId, branchID, FileId};
 			ResultSet rs = execPreparedQuery(sql, parms);
+			
+			// Create a map for <Commit_id, List<Change>>
+			Map<String, List<Change>> commitMap = new HashMap<String, List<Change>>();
+			LinkedList<Change> currentChanges = new LinkedList<Change>();
+			String currentCommit = "";
+			
 			while(rs.next())
 			{
-				changes.add(new Change(rs.getString("owner_id"), rs.getString("commit_id"), 
-						Resources.ChangeType.valueOf(rs.getString("change_type")), rs.getString("file_id"),
-						rs.getInt("line_start"), rs.getInt("line_end")));
+				String commitId = rs.getString("commit_id");
+				if(currentCommit.isEmpty())
+				{
+					// first commit
+					currentCommit = rs.getString("commit_id");
+					currentChanges.add(new Change(rs.getString("owner_id"),
+												  rs.getString("commit_id"), 
+												  Resources.ChangeType.valueOf(rs.getString("change_type")),
+												  rs.getString("file_id"),
+												  rs.getInt("char_start"),
+												  rs.getInt("char_end")));
+					
+				}
+				else 
+				{
+					if(commitId.equals(currentCommit))
+					{
+						// same commit, push into current map
+						currentChanges.add(new Change(rs.getString("owner_id"),
+													  rs.getString("commit_id"), 
+													  Resources.ChangeType.valueOf(rs.getString("change_type")),
+													  rs.getString("file_id"),
+													  rs.getInt("char_start"),
+													  rs.getInt("char_end")));
+					}
+					else
+					{
+						// add new File map
+						commitMap.put(currentCommit, currentChanges);
+						currentCommit = commitId;
+						currentChanges = new LinkedList<Change>();
+						currentChanges.add(new Change(rs.getString("owner_id"),
+													  rs.getString("commit_id"), 
+													  Resources.ChangeType.valueOf(rs.getString("change_type")),
+													  rs.getString("file_id"),
+													  rs.getInt("char_start"),
+													  rs.getInt("char_end")));
+					}
+				}
 			}
-			return changes;
+			
+			//Add last commit
+			if(!currentCommit.isEmpty())
+				commitMap.put(currentCommit, currentChanges);
+			
+			return commitMap;
 		}
 		catch(SQLException e) 
 		{
@@ -70,36 +119,32 @@ public class CallGraphDb extends DbConnection
 		}
 	}
 	
+	/**
+	 * Get onwer breakdown for a file for a specific commit. If the current commit does not have the file, look for its parent commit until find one.
+	 * @param FileId
+	 * @param CommitId
+	 * @return
+	 */
 	public List<Change> getAllOwnersForFileAtCommit(String FileId, String CommitId)
 	{
-		try 
+		LinkedList<Change> changes = new LinkedList<Change>();
+		
+		// Get commit path to the root from this commit
+		List<CommitFamily> commitPath = this.getCommitPathToRoot(CommitId);
+		
+		// get all the onwer entries for this file since this commit
+		Map<String, List<Change>> commitMap = getAllFileOwnerChangesBefore(FileId, CommitId);
+		
+		// For each commitFamily in commit Path, if we found an entry in OnwerEntries, grab it as it has the latest owner break down.
+		for(CommitFamily cf: commitPath)
 		{
-			LinkedList<Change> changes = new LinkedList<Change>();
-			String sql = "SELECT source_commit_id, file_id, owner_id, char_start, char_end, change_type FROM owners where commit_id=?" +
-					"and file_id=?;"; 
-			String[] parms = {CommitId, FileId};
-			ResultSet rs = execPreparedQuery(sql, parms);
-			if(!rs.next()) {
-				List<Commit> parents = getCommitParents(CommitId);
-				if(!parents.isEmpty()) {
-					return getAllOwnersForFileAtCommit(FileId, parents.get(0).getCommit_id());
-				}
-			}
-			else {
-				do
-				{
-					changes.add(new Change(rs.getString("owner_id"), rs.getString("source_commit_id"), 
-							Resources.ChangeType.valueOf(rs.getString("change_type")), rs.getString("file_id"),
-							rs.getInt("char_start"), rs.getInt("char_end")));
-				} while(rs.next());
-			}
-			return changes;
+			String commitId = cf.getChildId();
+			if(commitMap.containsKey(commitId))
+				return commitMap.get(commitId);
 		}
-		catch(SQLException e) 
-		{
-			e.printStackTrace();
-			return null;
-		}
+
+		// not found then return null
+		return null;
 	}
 	
 	public List<Commit> getCommitChildren(String CommitID) {
