ecff4f8510d1f17427cc4e3834b5b8658d2c3673
infiro <inferno71123@yahoo.com>
2012-06-14 16:27:26 -0700

    refractor codes

--- src/db/CallGraphDb.java
+++ src/db/CallGraphDb.java
@@ -21,34 +21,6 @@ public class CallGraphDb extends DbConnection
 		super();
 	}
 	
-	/**
-	 * Gets all of the change objects associated including and before a given commit.
-	 * @param CommitId
-	 * @return
-	 */
-	public List<Change> getAllOwnerChangesBefore(String CommitId)
-	{
-		try 
-		{
-			LinkedList<Change> changes = new LinkedList<Change>();
-			String sql = "SELECT commit_id, file_id, owner_id, char_start, char_end, change_type FROM owners natural join commits where commit_date <= (select commit_date from commits where commit_id=?)" +
-					"and (branch_id is NULL OR branch_id=?) order by commit_date, commit_id, char_start;"; 
-			String[] parms = {CommitId, branchID};
-			ResultSet rs = execPreparedQuery(sql, parms);
-			while(rs.next())
-			{
-				changes.add(new Change(rs.getString("owner_id"), rs.getString("commit_id"), Resources.ChangeType.valueOf(rs.getString("change_type")), rs.getString("file_id"), rs.getInt("char_start"), 
-						rs.getInt("char_end")));
-			}
-			return changes;
-		}
-		catch(SQLException e) 
-		{
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
 	public Map<String, List<Change>> getAllFileOwnerChangesBefore(String FileId, String CommitId)
 	{
 		try 
@@ -127,15 +99,13 @@ public class CallGraphDb extends DbConnection
 	 */
 	public List<Change> getAllOwnersForFileAtCommit(String FileId, String CommitId)
 	{
-		LinkedList<Change> changes = new LinkedList<Change>();
-		
 		// Get commit path to the root from this commit
 		List<CommitFamily> commitPath = this.getCommitPathToRoot(CommitId);
 		
 		// get all the onwer entries for this file since this commit
 		Map<String, List<Change>> commitMap = getAllFileOwnerChangesBefore(FileId, CommitId);
 		
-		// For each commitFamily in commit Path, if we found an entry in OnwerEntries, grab it as it has the latest owner break down.
+		// traverse comithPath if grab the first found as it has the latest owner break down.
 		for(CommitFamily cf: commitPath)
 		{
 			String commitId = cf.getChildId();
@@ -143,7 +113,6 @@ public class CallGraphDb extends DbConnection
 				return commitMap.get(commitId);
 		}
 
-		// not found then return null
 		return null;
 	}
 	
